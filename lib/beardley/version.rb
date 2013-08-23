0b6c0d38490a2a50be285d742cf0a28030608f5a
Brice Texier <burisu@oneiros.fr>
2013-08-23 17:37:37 +0200

    Bumps version to 0.5.0

--- lib/beardley/version.rb
+++ lib/beardley/version.rb
@@ -2,8 +2,8 @@
 module Beardley
   module VERSION
     MAJOR = 0
-    MINOR = 4
-    PATCH = 1
+    MINOR = 5
+    PATCH = 0
     PRE = nil
     
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
